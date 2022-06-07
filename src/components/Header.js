0df74122d2ead6f9d4096d8f4e67d83b4fcc27ed
athiraPrajod <athiraprajod1@gmail.com>
2022-06-07 16:50:47 +0530

    Background Images added

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css'
+import TeslaLogo from "../assets/teslaLogoSmall.svg"
+
+const Header = () =>
+{
+    return(
+        <div className = 'header'>
+            <div className = 'header__logo'>
+                <img src = {TeslaLogo} alt = "Tesla Logo"/>
+            </div>
+
+            <div className = "header__centre">
+                <p>Model S</p>
+                <p>Model 3</p>
+                <p>Model X</p>
+                <p>Model Y</p>
+                <p>Solar Roof</p>
+                <p>Solar Panel</p>
+            </div>
+
+            <div className = "header__right">
+                <p>Shop</p>
+                <p>Sign In</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
